8c44bd3ae6468a5e165912fe4ed009b30fe7e584
Ankit <ankit.ap.9932@gmail.com>
2023-05-17 00:09:54 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+// CSS for App
+import "./App.css";
+
+// importing components
+import Header from "./components/Header";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Testimonial from "./components/Testimonial";
+import Work from "./components/Work";
+import Menu from "./components/Menu";
+
+
+
+// importing router
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // METHOD - 1
+
+    // ADDING COMPONENTS NORMALLY
+    // using this all pages will be shown
+
+    <div className='App'>
+        <Header/>
+        <Home/>
+        <About/>
+        <Work/>
+        <Menu/>
+        <Testimonial/>
+        <Contact/>
+        <Footer/>
     </div>
+
+
+
+    // METHOD - 2
+
+    // ADDING COMPONENTS USING ROUTER
+    // using this only home pages will be shown with heder and footer connected to all pages
+    // we can switch to any page like localhost:3000/about
+    // Header and Footer are not wrapped in Router because they will be shown in each page
+    // <div className="App">
+    //   <BrowserRouter>
+    //     <Header />
+
+    //     <Routes>
+    //       <Route path="/" element={<Home />} />
+    //       <Route path="/about" element={<About />} />
+    //       <Route path="/Work" element={<Work />} />
+    //       <Route path="/Testimonial" element={<Testimonial />} />
+    //       <Route path="/contact" element={<Contact />} />
+    //     </Routes>
+
+    //     <Footer />
+    //   </BrowserRouter>
+    // </div>
   );
 }
 
 export default App;
+
+// for icons install below
+// npm install react-icons --save
---
+++ src/components/Footer.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+// importing logo from local
+import Logo from "../Assets/Logo.svg";
+
+//importing icons
+import { BsYoutube } from "react-icons/bs";
+import { BsTwitter } from "react-icons/bs";
+import { SiLinkedin } from "react-icons/si";
+import { FaFacebookF } from "react-icons/fa";
+
+function Footer() {
+  return (
+    <div className="footer-wrapper">
+      <div className="footer-section-one">
+        <div className="footer-logo-container">
+          <img src={Logo} alt="" />
+        </div>
+        <div className="footer-icons">
+          <a href="https://www.youtube.com/" target="_blank"><BsYoutube /></a>
+          <a href="https://www.youtube.com/" target="_blank"><SiLinkedin /></a>
+          <a href="https://www.youtube.com/" target="_blank"><FaFacebookF /></a>
+          <a href="https://www.youtube.com/" target="_blank"><BsTwitter /></a> 
+        </div>
+      </div>
+      <div className="footer-section-two">
+        <div className="footer-section-columns">
+          <span>Qualtiy</span>
+          <span>Help</span>
+          <span>Share</span>
+          <span>Carrers</span>
+          <span>Testimonials</span>
+          <span>Work</span>
+        </div>
+        <div className="footer-section-columns">
+          <span>244-5333-7783</span>
+          <span>hello@food.com</span>
+          <span>press@food.com</span>
+          <span>contact@food.com</span>
+        </div>
+        <div className="footer-section-columns">
+          <span>Terms & Conditions</span>
+          <span>Privacy Policy</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
